c72bf9b4f6f3ef8add3ec8b6697a934387296e89
martin.bachvarov <martin.bachvarov@estafet.com>
2016-11-08 13:36:45 +0000

    changes

--- app/src/main/java/com/martinbachvarov/fpmi/MainActivity.java
+++ app/src/main/java/com/martinbachvarov/fpmi/MainActivity.java
@@ -1,14 +1,25 @@
 package com.martinbachvarov.fpmi;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import layout.HomeLoginFragment;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openHomePageFragment();
+    }
+
+    private void openHomePageFragment() {
+        FragmentTransaction fTransaction = getSupportFragmentManager().beginTransaction();
+        fTransaction.replace(R.id.container, new HomeLoginFragment());
+        fTransaction.commit();
     }
 
     @Override
