29b4cb9acc51bbc31071331e02c1bf2b510e8299
martin.bachvarov <martin.bachvarov@estafet.com>
2016-11-14 23:44:05 +0000

    Add spinners values and validation for name input

--- app/src/main/java/layout/HomeLoginFragment.java
+++ app/src/main/java/layout/HomeLoginFragment.java
@@ -6,12 +6,23 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.martinbachvarov.fpmi.R;
 
-public class HomeLoginFragment extends Fragment {
+public class HomeLoginFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    String[] degree = {"Bachelor", "Magister"};
+    String[] bachSpeciality = {"Еngineering Physics", "Applied Mathematics", "Mathematics and Informatics"};
+    String[] magisterSpeciality = {"Applied Mathematics"};
+    String[] bachCourse = {"First", "Second", "Third", "Fourth"};
+    String[] magisterCourse = {"First"};
     private Button btnContinue;
+    private EditText nameInput;
+    private Spinner degreeSpnr, specialitySpnr, courseSpnr;
 
 
     @Override
@@ -19,7 +30,23 @@ public class HomeLoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_login_fragment, container, false);
 
+        nameInput = (EditText) view.findViewById(R.id.homeNameInput);
+
         btnContinue = (Button) view.findViewById(R.id.buttonHomeContinue);
+
+        degreeSpnr = (Spinner) view.findViewById(R.id.selectDegreeSpinner);
+        degreeSpnr.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        ArrayAdapter<String> degreeAdapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_spinner_item, degree);
+        degreeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        degreeSpnr.setAdapter(degreeAdapter);
+
+        specialitySpnr = (Spinner) view.findViewById(R.id.specialitySpinner);
+        specialitySpnr.setEnabled(false);
+
+        courseSpnr = (Spinner) view.findViewById(R.id.selectCourseSpinner);
+        courseSpnr.setEnabled(false);
+
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,9 +57,58 @@ public class HomeLoginFragment extends Fragment {
         return view;
     }
 
+    //Check is name input null
+    private boolean checkNameInputIsNull() {
+        if (nameInput.getText().toString().trim().length() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private void openFragment() {
-        FragmentTransaction fTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-        fTransaction.replace(R.id.container, new MenuPageFragment());
-        fTransaction.commit();
+        if (checkNameInputIsNull()) {
+            nameInput.setError("First name is required");
+        } else {
+            //Get username value from home page and display it on menu page
+            MenuPageFragment fragment = new MenuPageFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("username", nameInput.getText().toString());
+            fragment.setArguments(bundle);
+
+            //Replace HomeLoginFragment with MenuPageFragment
+            FragmentTransaction fTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+            fTransaction.replace(R.id.container, fragment);
+            fTransaction.commit();
+        }
+    }
+
+    //Fill data into Spinners depends on user choice- Bachelor or Magister
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        specialitySpnr.setEnabled(true);
+        courseSpnr.setEnabled(true);
+        if (degreeSpnr.getSelectedItem().equals("Bachelor")) {
+            ArrayAdapter<String> specialityData = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, bachSpeciality);
+            specialityData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            specialitySpnr.setAdapter(specialityData);
+
+            ArrayAdapter<String> courseData = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, bachCourse);
+            courseData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            courseSpnr.setAdapter(courseData);
+        } else if (degreeSpnr.getSelectedItem().equals("Magister")) {
+            ArrayAdapter<String> specialityData = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, magisterSpeciality);
+            specialityData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            specialitySpnr.setAdapter(specialityData);
+
+            ArrayAdapter<String> courseData = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, magisterCourse);
+            courseData.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            courseSpnr.setAdapter(courseData);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/layout/MenuPageFragment.java
+++ app/src/main/java/layout/MenuPageFragment.java
@@ -11,11 +11,21 @@ import com.martinbachvarov.fpmi.R;
 
 
 public class MenuPageFragment extends Fragment {
+    private TextView nameView;
+    private static final String HI_MESSAGE = "Hi";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu_page, container, false);
+        nameView = (TextView) view.findViewById(R.id.nameView);
+
+        setNameView();
         return view;
+    }
+
+    private void setNameView() {
+        String value = getArguments().getString("username");
+        nameView.setText(HI_MESSAGE + ", " + value);
     }
 }
